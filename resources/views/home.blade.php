d1a3c249c55e4d8f47ec9134d5d41ddffa7248b0
M3YU <82hank@gmail.com>
2022-01-18 09:24:58 +0800

    後臺頁簽追加

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -256,7 +256,6 @@
             </div>
         </div>
 
-        <!-- Pending Requests Card Example -->
         <div class="col-xl-3 col-md-6 mb-4">
             <div class="card border-left-info shadow h-100 py-2">
                 <div class="card-body">
@@ -264,7 +263,9 @@
                         <div class="col mr-2">
                             <div class="text-xs font-weight-bold text-info text-uppercase mb-1">
                                 前台頁面</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800"> <a href="{{route('contact.index')}}">聯絡我們</a> </div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">
+                                <a href="{{route('services')}}">服務頁面</a>
+                            </div>
                         </div>
                         <div class="col-auto">
                             <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
