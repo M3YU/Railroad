c642c1fd5ac22d6a1abbf82c2c5d1d0b3ddb09db
M3YU <82hank@gmail.com>
2022-01-19 21:20:55 +0800

    後臺補充頁面

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -283,6 +283,26 @@
                             <div class="text-xs font-weight-bold text-info text-uppercase mb-1">
                                 前台頁面</div>
                             <div class="h5 mb-0 font-weight-bold text-gray-800">
+                                <a href="{{route('teams')}}">揪團</a>
+                            </div>
+                        </div>
+                        <div class="col-auto">
+                            <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-info shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-info text-uppercase mb-1">
+                                前台頁面</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">
                                 <a href="{{route('services')}}">服務頁面</a>
                             </div>
                         </div>
