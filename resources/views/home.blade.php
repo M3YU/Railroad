d5a80f3f34ae6b48fa83b1df630ec6392fa2f6d2
M3YU <82hank@gmail.com>
2022-01-19 21:26:23 +0800

    微調

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -293,7 +293,7 @@
                 </div>
             </div>
         </div>
-    </div>
+    
 
         <div class="col-xl-3 col-md-6 mb-4">
             <div class="card border-left-info shadow h-100 py-2">
